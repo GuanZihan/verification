3fb6bb82fd708bbf281743b794305d56f698f1dc
jmgzh@outlook.com <jmgzh123@outlook.com>
2021-08-16 03:42:16 +0100

    update project

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 from Examples import test_taxi
 
-file = ["Neural Network/MNIST/mnist10x20.nnet", "Neural Network/MNIST/mnist20x20.nnet", "Neural Network/MNIST/mnist20x40.nnet"]
-file_Auto_Taxi = ["Neural Network/AutoTaxi/AutoTaxi_32Relus_200Epochs_OneOutput.nnet"]
+file_Auto_Taxi = ["Neural Network/AutoTaxi/AutoTaxi_32Relus_200Epochs_OneOutput.nnet", "Neural Network/AutoTaxi/AutoTaxi_64Relus_200Epochs_OneOutput.nnet", "Neural Network/AutoTaxi/AutoTaxi_128Relus_200Epochs_OneOutput.nnet"]
 # dims = [[784, 20, 10], [784, 50, 10], [784, 50, 50, 10], [784, 100, 100, 10]]
 epss = [0.04, 0.08, 0.016]
 for eps in epss:
