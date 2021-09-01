216b9dfa99723561ad5cffbff211e4c112084250
jmgzh@outlook.com <jmgzh123@outlook.com>
2021-09-01 04:28:00 +0100

    add comment

--- Utils.py
+++ Utils.py
@@ -7,7 +7,9 @@ import scipy.io as scio
 
 
 def open_file(name, *open_args, **open_kwargs):
-    """Load file, downloading to /tmp/jax_verify first if necessary."""
+    """
+    Load file function from 'Enabling certification of verification-agnostic networks via memory-efficient semidefinite programming'
+    """
     local_path = "./" + name
     if not os.path.exists(os.path.dirname(local_path)):
         os.makedirs(os.path.dirname(local_path))
@@ -27,40 +29,6 @@ def process_bounds(bounds):
         Y_max.append(np.squeeze(bound[3].T, axis=1))
 
     return Y_min, Y_max, X_min, X_max
-
-
-def read_nn(file_name):
-    """
-    parse the .nnet file, extract properties such as dims, weights, bias, input_min, input_max.
-    :param file_name: file to be read
-    :return: dims, weights, bias, input_min, input_max
-    """
-    with open(file_name, "r") as f:
-        line = f.readline()
-        while line.startswith("//"):
-            line = f.readline()
-        layers, dim_in, dim_out, neuronsize = eval(line)
-        dims = list(eval(f.readline()))
-        f.readline()  # A flag that is no longer used
-        input_min = np.array(list(eval(f.readline())))
-        input_max = np.array(list(eval(f.readline())))
-        f.readline()
-        f.readline()
-        weights = []
-        bias = []
-        for i in dims[1:]:
-            current_layer = i
-            weights_layer = []
-            bias_layer = []
-            for j in range(current_layer):
-                weights_layer.append(list(eval(f.readline())))
-            for j in range(current_layer):
-                bias_layer.append(list(eval(f.readline())))
-            weights_layer = np.array(weights_layer)
-            bias_layer = np.array(bias_layer)
-            weights.append(weights_layer)
-            bias.append(bias_layer)
-        return dims, weights, bias, input_min, input_max
 
 
 def write_single_data_to_matlab_path(filename, key, data):
